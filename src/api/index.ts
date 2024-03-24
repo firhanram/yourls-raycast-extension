c25ad2b7bb18476db8ac7e9e366103a52984bba3
Firhan Ramadhan <rfirhan99@gmail.com>
2024-03-24 14:42:32 +0700

    fix: add action type

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,10 @@
 import { yourls } from "../lib/yourls";
-import { GetCreateShortUrlResponse, TCreateShortUrlParams } from "../types";
+import { GetCreateShortUrlResponse, TAction, TCreateShortUrlParams } from "../types";
 
 export const createShortUrl = async (params: TCreateShortUrlParams) => {
+  const action: TAction = "shorturl";
   const res = await yourls<GetCreateShortUrlResponse>({
-    action: "shorturl",
+    action,
     params,
   });
 
